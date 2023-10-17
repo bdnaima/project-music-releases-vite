5885f0edaba83b2346008d6d1f7f9cc27ae0c38c
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-10-17 14:03:57 +0200

    added a bit of hover effect and some styling

--- src/components/CoverImage.jsx
+++ src/components/CoverImage.jsx
@@ -1,5 +1,19 @@
 import data from "../data.json";
+import heartIcon from "../assets/icons/heart.svg";
+import playIcon from "../assets/icons/play.svg";
+import dotsIcon from "../assets/icons/dots.svg";
+import "../styles/CoverImage.css";
 
 export function CoverImage(props) {
-  return <img src={props.src} alt={props.alt} />;
+  return (
+    <div className="image-container">
+    <img src={props.src} alt={props.alt} />
+    <div className="icons-container">
+      <img src={heartIcon} alt="heart-icon" className="icons"></img>
+      <img src={playIcon} alt="play-icon" className="icons"></img>
+      <img src={dotsIcon} alt="dots-icon" className="icons"></img>
+    </div>
+    </div>
+  )
+ 
 }
