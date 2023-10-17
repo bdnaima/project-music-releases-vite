59c5ee0690388d820aa604ae373d3f68c353a534
Naima Malik <noma_m@hotmail.com>
2023-10-17 14:26:03 +0200

    Move style changes for albums

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -2,20 +2,18 @@ import React from "react";
 import { CoverImage } from "./CoverImage";
 import { AlbumName } from "./AlbumName";
 import { ArtistName } from "./ArtistName";
-import "../styles/Album.css"
+import "../styles/Album.css";
 
 export function Album({ albums }) {
   return (
-    <div className="album-grid d-flex">
-        {albums.map((album) => (
-           <div className="albums" key={album.id}>
-            <CoverImage src={album.images[0].url} alt={album.name} />
-            <AlbumName name={album.name} className="album-item"/>
-            <ArtistName name={album.artists[0].name} className="album-item"/>
-            </div>
-        ))}
-     
-
+    <div className="album-grid">
+      {albums.map((album) => (
+        <div className="albums" key={album.id}>
+          <CoverImage src={album.images[0].url} alt={album.name} />
+          <AlbumName name={album.name} className="album-item" />
+          <ArtistName name={album.artists[0].name} className="album-item" />
+        </div>
+      ))}
     </div>
   );
 }
