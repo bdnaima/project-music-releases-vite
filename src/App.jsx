b253d6afeac0394fe73a7931f3195b9415c41478
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-10-18 08:56:42 +0200

    created header component and added it to the album component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Album } from "./components/Album";
 export function App() {
   return (
     <>
-      <h1>New Albums & Singles</h1>
       <Album albums={data.albums.items} />
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export function Header() {
+  return (
+    <h1>New Albums & Singles</h1>
+  )
+}
+
