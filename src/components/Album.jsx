90c71a3077962a4f98c02f33a811c1a07dd8ae1d
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-10-18 14:17:28 +0200

    done with 2 to 3 artistName updates

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -8,28 +8,41 @@ import "../styles/Album.css";
 export function Album({ albums }) {
   return (
     <>
-    <Header />
-    <div className="album-grid">
-      {albums.map((album) => (
-        <div className="albums" key={album.id}>
-          <CoverImage src={album.images[0].url} alt={album.name} />
-          <a
-            href={album.external_urls.spotify}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <AlbumName name={album.name} className="album-item" />
-          </a>
-          <a
-            href={album.artists[0].external_urls.spotify}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <ArtistName name={album.artists[0].name} className="album-item" />
-          </a>
-        </div>
-      ))}
-    </div>
+      <Header />
+      <div className="album-grid">
+        {albums.map((album) => (
+          <div className="albums" key={album.id}>
+            <CoverImage src={album.images[0].url} alt={album.name} />
+            <a
+              href={album.external_urls.spotify}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <AlbumName name={album.name} className="album-item" />
+            </a>
+            <a
+              href={album.artists[0].external_urls.spotify}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ArtistName
+                name={album.artists.map((aName, index, array) => {
+                  if (array.length === 2) {
+                    return index === 0 ? `${aName.name} & ` : aName.name;
+                  } else if (index === array.length - 1) {
+                    return `${aName.name}`;
+                  } else if (index === array.length - 2) {
+                    return `${aName.name}, `;
+                  } else {
+                    return aName.name;
+                  }
+                })}
+                className="album-item"
+              />
+            </a>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
