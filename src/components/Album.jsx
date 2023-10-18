f8f8d903764c050acfd6bd80925bb901f02eadef
Naima Malik <noma_m@hotmail.com>
2023-10-18 15:13:56 +0200

    Change albums container to grid

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -8,28 +8,28 @@ import "../styles/Album.css";
 export function Album({ albums }) {
   return (
     <>
-    <Header />
-    <div className="album-grid">
-      {albums.map((album) => (
-        <div className="albums" key={album.id}>
-          <CoverImage src={album.images[0].url} alt={album.name} />
-          <a
-            href={album.external_urls.spotify}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <AlbumName name={album.name} className="album-item" />
-          </a>
-          <a
-            href={album.artists[0].external_urls.spotify}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <ArtistName name={album.artists[0].name} className="album-item" />
-          </a>
-        </div>
-      ))}
-    </div>
+      <Header />
+      <div className="album-grid">
+        {albums.map((album) => (
+          <div className="albums" key={album.id}>
+            <CoverImage src={album.images[0].url} alt={album.name} />
+            <a
+              href={album.external_urls.spotify}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <AlbumName name={album.name} className="album-item" />
+            </a>
+            <a
+              href={album.artists[0].external_urls.spotify}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ArtistName name={album.artists[0].name} className="album-item" />
+            </a>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
