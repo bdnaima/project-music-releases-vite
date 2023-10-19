8a6d519ebfe253c59f271579f7118c6a779520d1
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-10-19 13:11:25 +0200

    fixed the artist names with & and , also test by adding some more artist name in data.json file

--- src/components/ArtistName.jsx
+++ src/components/ArtistName.jsx
@@ -5,23 +5,18 @@ export function ArtistName(props) {
   const { artists } = props;
 
   const formattedNames = artists.map((artistName, index, array) => {
-    if (array.length === 1) {
+    if (index === 0) {
       return artistName.name;
-    } else if (array.length === 2) {
-      return index === 0 ? `${artistName.name} & ` : artistName.name;
     } else if (index === array.length - 1) {
       return ` & ${artistName.name}`;
-    } else if (index === array.length - 2) {
-      return `${artistName.name}, `;
     } else {
-      return artistName.name;
+      return `, ${artistName.name}`;
     }
   });
 
   return (
     <div className="artist-name">
-    <h3>{formattedNames}</h3>
+      <h3>{formattedNames}</h3>
     </div>
   );
 };
-
