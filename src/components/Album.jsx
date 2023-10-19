80d7f2fe4b0ec23c07326106de752a5db289a632
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-10-19 12:45:01 +0200

    filter single and new albums in two different section

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -7,31 +7,47 @@ import "../styles/Album.css";
 import { Sidebar } from "./Sidebar";
 
 export function Album({ albums }) {
+  //separating the albums into single and album types
+  const singleAlbums = albums.filter((album) => album.album_type === "single");
+  const newAlbums = albums.filter((album) => album.album_type === "album");
+
+  //function that displays the music data
+  function renderAlbums(albumsToRender) {
+    return (
+      <div className="album-grid">
+        {albumsToRender.map((album) => (
+          <div className="album" key={album.id}>
+            <CoverImage src={album.images[0].url} alt={album.name} />
+            <a
+              href={album.external_urls.spotify}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <AlbumName name={album.name} className="album-item" />
+            </a>
+            <a
+              href={album.artists[0].external_urls.spotify}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ArtistName artists={album.artists} className="album-item" />
+            </a>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <>
       <Header />
       <div className="album-sidebar-container">
         <Sidebar />
-        <div className="album-grid">
-          {albums.map((album) => (
-            <div className="album" key={album.id}>
-              <CoverImage src={album.images[0].url} alt={album.name} />
-              <a
-                href={album.external_urls.spotify}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AlbumName name={album.name} className="album-item" />
-              </a>
-              <a
-                href={album.artists[0].external_urls.spotify}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <ArtistName artists={album.artists} className="album-item" />
-              </a>
-            </div>
-          ))}
+        <div>
+          <h2 className="sub-heading">Singles</h2>
+          {renderAlbums(singleAlbums)}
+          <h2 className="sub-heading">New Albums</h2>
+          {renderAlbums(newAlbums)}
         </div>
       </div>
     </>
